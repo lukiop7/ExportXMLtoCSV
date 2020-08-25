8ba713573b0421376c12d513b170ff8f230afcc0
lukiop7 <lukiop111@poczta.fm>
2020-08-25 11:43:52 +0200

    create basic classes for deserialization

---
+++ ExportXMLtoCSV/ExportXMLtoCSV/ObjectClasses.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ExportXMLtoCSV
+{
+    namespace ObjectClasses
+    {
+        [XmlRoot("ProductionReport")]
+        public class ProductionReport
+        {
+            [XmlAttribute("Date")]
+            public string Date { get; set; }
+
+            [XmlAttribute("Manufacturer")]
+            public string Manufacturer { get; set; }
+
+            [XmlArray("Factories")]
+            [XmlArrayItem("Factory")]
+            public List<Factory> Factories { get; set; }
+        }
+
+        public class Factory
+        {
+            [XmlAttribute("Name")]
+            public string Name { get; set; }
+
+            [XmlArray("ProducedCars")]
+            [XmlArrayItem("Car")]
+            public List<Car> Cars { get; set; }
+        }
+
+        public class Car
+        {
+            [XmlAttribute("VIN")]
+            public string VIN { get; set; }
+
+            public int ProductionYear { get; set; }
+
+            public string Model { get; set; }
+
+            [XmlArray("Features")]
+            [XmlArrayItem("Feature")]
+            public List<Feature> Features { get; set; }
+        }
+
+        public class Feature
+        {
+            [XmlAttribute("Code")] 
+            public string Code { get; set; }
+
+
+        }
+    }
+}
---
+++ ExportXMLtoCSV/ExportXMLtoCSV/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Xml.Serialization;
+using ExportXMLtoCSV.ObjectClasses;
+
+namespace ExportXMLtoCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(ProductionReport));
+            using (FileStream fileStream = new FileStream(@"D:\Projekty_Wakacje\ExportXMLtoCSV\ExportXMLtoCSV\ExportXMLtoCSV\ProductionResults.xml", FileMode.Open))
+            {
+                ProductionReport result = (ProductionReport)serializer.Deserialize(fileStream);
+                foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(result))
+                {
+                    string name = descriptor.Name;
+                    object value = descriptor.GetValue(result);
+                    Console.WriteLine("{0}={1}", name, value);
+                }
+            }
+        }
+    }
+}
